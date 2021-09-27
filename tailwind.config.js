0c44461710f2674c724c3e696fcb11705ec44ae5
AlvinJS <manzi1alvin@gmail.com>
2021-09-27 15:21:36 +0200

    ----It has been commited-----

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+      "custom-background-image-1":"url('/images/Car.jpg')",
+      "custom-background-image-2":"url('/images/Lingard.png')",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
